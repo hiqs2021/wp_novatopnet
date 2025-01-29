0a94d66640e4e9fa29bf12aad08ec6e4dba89939
Novatopnet <72166224+hiqs2021@users.noreply.github.com>
2025-01-29 07:15:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
+/**
+ * Galeria de Imagens para postagens no wordpress start.
+*
+* @since 2025
+*/
 
+<div class="gallery-container">
+  <?php
+    $args = array(
+      'posts_per_page' => 9,
+      'post_status'    => 'publish',
+    );
+
+    $recent_posts = new WP_Query($args);
+
+    if ($recent_posts->have_posts()) :
+      while ($recent_posts->have_posts()) : $recent_posts->the_post();
+  ?>
+  <div class="gallery-item">
+    <?php if (has_post_thumbnail()) : ?>
+      <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" loading="lazy">
+    <?php endif; ?>
+    <div class="overlay">
+      <h3><?php the_title(); ?></h3>
+      <p><?php echo wp_trim_words(get_the_excerpt(), 15, '...'); ?></p>
+      <span class="badge"><?php echo get_the_date(); ?></span>
+    </div>
+  </div>
+  <?php
+      endwhile;
+      wp_reset_postdata();
+    endif;
+  ?>
+</div>
